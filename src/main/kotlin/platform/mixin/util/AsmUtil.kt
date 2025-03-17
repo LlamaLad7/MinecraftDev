eedb0c0e036ae4a8bd42641e4a0bdaffe659fffa
LlamaLad7 <l3gomindstorms@gmail.com>
2025-03-17 19:43:28 +0000

    Fix: Resolve compile errors from new 2025.1 build.

--- src/main/kotlin/platform/mixin/util/AsmUtil.kt
+++ src/main/kotlin/platform/mixin/util/AsmUtil.kt
@@ -181,7 +181,7 @@ val ClassNode.shortName
     get() = internalNameToShortName(name)
 
 private val LOAD_CLASS_FILE_BYTES: Method? = runCatching {
-    com.intellij.byteCodeViewer.BytecodeViewerManager::class.java
+    com.intellij.byteCodeViewer.ByteCodeViewerManager::class.java
         .getDeclaredMethod("loadClassFileBytes", PsiClass::class.java)
         .let { it.isAccessible = true; it }
 }.getOrNull()
